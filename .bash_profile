ee5e43cb5d64d7cb993e2ffba52bf28ea8b8f00d
Albert Johannessen <albertjohannessen@live.no>
2020-05-05 17:11:17 +0200

    Alt-Tab to i3

--- .bash_profile
+++ .bash_profile
@@ -6,6 +6,7 @@
 
 # export PATH=$PATH:$HOME/.scripts
 export EDITOR="vim"
+export TERM="alacritty"
 export TERMINAL="alacritty"
 export BROWSER="firefox"
 
--- LowBattery.c
+++ LowBattery.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(){
+	FILE* file;
+	char batteryString[4];
+	int battery;
+
+	char statusString[20];
+
 	while(1){
-		FILE* file = fopen("/sys/class/power_supply/BAT1/capacity", "r");
-		char string[4];
-		fgets(string, 4, file);
+		file = fopen("/sys/class/power_supply/BAT1/capacity", "r");
+		fgets(batteryString, 4, file);
 		fclose(file);
-		int battery;
-		sscanf(string, "%d", &battery);
-		printf("%d\n", battery);
+		sscanf(batteryString, "%d", &battery);
 		if(battery < 20){
 			system("i3 workspace 10");
 		}
 		sleep(60);
+
+		file = fopen("/sys/class/power_supply/BAT1/status", "r");
+		fgets(statusString, 20, file);
+		fclose(file);
+		if(strcmp(statusString, "Charging\n") == 0){
+			return 0;
+		}
 	}
 	return 0;
 }
