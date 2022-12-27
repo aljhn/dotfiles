4a5b2bf681950a3f9761097162384fa1ce9cdef8
Albert Johannessen <albertjohannessen@live.no>
2022-12-27 19:44:51 +0100

    More neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,19 +8,23 @@ if not vim.loop.fs_stat(lazypath) then
     "https://github.com/folke/lazy.nvim.git",
     lazypath,
   })
-end
-vim.opt.runtimepath:prepend(lazypath)
+end vim.opt.runtimepath:prepend(lazypath)
 
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set({"n", "v"}, '<Space>', '<Nop>', {silent = true})
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.breakindent = true
+vim.opt.backspace = "indent,eol,start"
 
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -45,14 +49,32 @@ local plugins = {
 				highlight = {
 					enable = true,
                     additional_vim_regex_highlighting = false
-				}
+				},
+                indent = {
+                    enable = true
+                }
 			}
 		end
-	},
+    },
+	{
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup {
+                char = "┊",
+                show_trailing_blankline_indent = true
+            }
+        end
+    },
     "nvim-tree/nvim-web-devicons",
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
+        branch = "0.1.x",
         dependencies = {
             "nvim-lua/plenary.nvim"
         },
@@ -92,7 +114,49 @@ local plugins = {
     {
         "nvim-lualine/lualine.nvim",
         config = function()
-            require("lualine").setup ()
+            require("lualine").setup {
+                options = {
+                    component_separators = "|",
+                    section_separators = ""
+                }
+            }
+        end
+    },
+    {
+        "akinsho/bufferline.nvim",
+        version = "v3.*",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        config = function()
+            require("bufferline").setup()
+            vim.keymap.set("n", "<leader>q", ":bd<CR>")
+            vim.keymap.set("n", "<leader>h", ":BufferLineCyclePrev<CR>")
+            vim.keymap.set("n", "<leader>l", ":BufferLineCycleNext<CR>")
+        end
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            vim.cmd([[let g:neo_tree_remove_legacy_commands = 1]])
+            vim.keymap.set("n", "<leader>e", ":Neotree<CR>")
+            require("neo-tree").setup {
+                window = {
+                    width = 30
+                }
+            }
+        end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
         end
     },
 	{
@@ -103,9 +167,18 @@ local plugins = {
 				style = "dark",
 				transparent = true
 			}
-			--require("onedark").load()
 		end
-	}
+	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        config = function()
+            require("tokyonight").setup {
+                style = "storm",
+                transparent = true
+            }
+        end
+    },
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
