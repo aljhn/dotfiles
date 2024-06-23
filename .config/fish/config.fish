3c3e9ddfd30518902edec91b51e21763dd72495a
Albert Johannessen <albertjohannessen@live.no>
2024-06-23 23:51:09 +0200

    Updated alacritty config. Start replacing hypr applications.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,6 @@ set fish_greeting
 alias l="exa -l -a --icons --color=always --group-directories-first"
 alias integrated="sudo envycontrol --switch integrated; reboot"
 alias hybrid="sudo envycontrol --switch hybrid; reboot"
-alias sxiv="sxiv -b"
 
 alias neofetch="set SHELL /bin/fish; /usr/bin/neofetch; set SHELL /bin/bash"
 
