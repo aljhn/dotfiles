595d1d8654569060b27b678b00f7ebd2d56edbe6
Albert Johannessen <albertjohannessen@live.no>
2024-06-24 02:03:06 +0200

    Finalized hypr setup.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,6 @@ alias l="exa -l -a --icons --color=always --group-directories-first"
 alias integrated="sudo envycontrol --switch integrated; reboot"
 alias hybrid="sudo envycontrol --switch hybrid; reboot"
 
-alias neofetch="set SHELL /bin/fish; /usr/bin/neofetch; set SHELL /bin/bash"
-
 zoxide init fish | source
 
 fish_add_path ~/.local/bin
