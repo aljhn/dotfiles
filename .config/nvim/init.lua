dcfb7b6d635f788a53efe73873694920c3505551
Albert Johannessen <albertjohannessen@live.no>
2024-07-15 04:03:01 +0200

    Updated neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -46,54 +46,298 @@ vim.opt.mouse = "a"
 require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
         build = ":TSUpdate",
-        config = function () 
+        dependencies = {
+            "windwp/nvim-ts-autotag",
+        },
+        config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "python", "c", "cpp", "haskell" },
+                ensure_installed = {
+                    "lua",
+                    "bash",
+                    "gitignore",
+                    "python",
+                    "c",
+                    "cpp",
+                    "haskell",
+                    "cmake",
+                    "html",
+                    "css",
+                    "javascript",
+                    "json",
+                    "toml",
+                },
                 sync_install = false,
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
                 },
                 indent = {
-                    enable = true
+                    enable = true,
+                },
+                autotag = {
+                    enable = true,
                 },
             })
-        end
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            },
+            "nvim-tree/nvim-web-devicons",
+            "folke/todo-comments.nvim",
+        },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [G]rep" })
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+            vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "[F]ind [T]odos" })
+
+            require("telescope").load_extension("fzf")
+        end,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            {
+                "L3MON4D3/LuaSnip",
+                version = "v2.*",
+                build = "make install_jsregexp",
+            },
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            local cmp = require("cmp")
+            cmp.setup({
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+
+            local mason_lspconfig = require("mason-lspconfig")
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                },
+            })
+
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "stylua",
+                    "isort",
+                    "black",
+                    "pylint",
+                },
+            })
+
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            mason_lspconfig.setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilites,
+                    })
+                end,
+                ["lua_ls"] = function()
+                    lspconfig["lua_ls"].setup({
+                        capabilities = capabilites,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" },
+                                    disable = { "missing-fields" },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                            },
+                        },
+                    })
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(event)
+                    local map = function(keys, func, desc)
+                        vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+                    end
+
+                    local builtin = require("telescope.builtin")
+                    map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+                    map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+                    map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+                    map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+                    map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+                    map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+                    map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                    map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                    map("K", vim.lsp.buf.hover, "Hover Documentation")
+                    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+                end,
+            })
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        keys = {
+            {
+                "<leader>F",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
+                mode = "n",
+                desc = "[F]ormat file",
+            },
+        },
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "black" },
+            },
+            -- format_on_save = {
+            --     timeout_ms = 500,
+            --     lsp_format = "fallback"
+            -- }
+        },
+    },
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local lint = require("lint")
+            lint.linters_by_ft = {
+                python = { "pylint" },
+            }
+
+            local lint_augrup = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augrup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            })
+        end,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {},
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 1000
+        end,
+    },
+    {
+        "folke/todo-comments.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {},
     },
     {
         "numToStr/Comment.nvim",
-        opts = {}
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {},
     },
     {
         "lewis6991/gitsigns.nvim",
-        opts = {}
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {},
     },
     {
         "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPre", "BufNewFile" },
         main = "ibl",
         opts = {
             indent = {
                 char = "┊",
-            }
-        }
+            },
+        },
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
     },
     {
         "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
         opts = {
             options = {
                 component_separators = "|",
                 section_separators = "",
-            }
-        }
+            },
+        },
     },
     {
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-        opts = { transparent = true }
-    }
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+        },
+    },
 })
 
 vim.cmd([[colorscheme kanagawa]])
